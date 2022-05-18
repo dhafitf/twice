b9ed45f2ba911dd05ac79754e9f49a57d3eab126
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-05-18 13:14:43 +0700

    update footer components

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,51 @@
 import styles from "~styles/footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { Container } from "../layout";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href="https://github.com/dhafitf" target="_blank" rel="noopener noreferrer">
-        Dhafit Farenza
-      </a>
+      <Container className={styles.container}>
+        <Link href="/">
+          <a className={styles.logo}>
+            <Image src="/logo.svg" alt="logo" width={50} height={50} />
+          </a>
+        </Link>
+        <div className={styles.link}>
+          <div className={styles.official_site}>
+            <a href="https://twice.jype.com/" target="_blank" rel="noreferrer">
+              TWICE Korea Official Website
+            </a>
+            <a href="https://twicejapan.com" target="_blank" rel="noreferrer">
+              TWICE Japan Official Website
+            </a>
+          </div>
+          <div className={styles.other}>
+            <Link href="/developers">
+              <a>For Developers</a>
+            </Link>
+          </div>
+        </div>
+      </Container>
+      <div className={styles.credit}>
+        <p>
+          Built by{" "}
+          <a href="https://dhafit.xyz/" target="_blank" rel="noreferrer">
+            Dhafit Farenza
+          </a>
+        </p>
+        <p>
+          All copyright reserved to{" "}
+          <a href="https://www.jype.com/" target="_blank" rel="noreferrer">
+            JYPE
+          </a>
+          {" & "}
+          <a href="https://wmg.jp/" target="_blank" rel="noreferrer">
+            Sony Music
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
