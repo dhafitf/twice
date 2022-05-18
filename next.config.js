1bc40dba79f995cd5dd2b6d50a5ce46ac2a22721
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-05-18 10:28:59 +0700

    add new dependecies & additional settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  skipLibCheck: true,
+  images: {
+    domains: ["i.ytimg.com", "lv2-cdn.azureedge.net", "s3-aop.plusmember.jp", "scontent.cdninstagram.com", "v.phinf.naver.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
