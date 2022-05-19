0b84029c50430fbd21c18c853f8c96170da397e2
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-05-19 15:22:27 +0700

    Update dbConnect.ts

--- src/lib/utils/dbConnect.ts
+++ src/lib/utils/dbConnect.ts
@@ -1,9 +1,40 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = `${process.env.MONGO_URI}`;
+const MONGODB_URI = `${process.env.MONGO_URI}`;
 
-if (!MONGO_URI) {
-  throw new Error("Please define the MONGO_URI environment variable inside .env.local");
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-export const dbConnect = () => mongoose.connect(MONGO_URI);
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+declare const global: CustomNodeJsGlobal;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
