95b198d212f2cdfaee0a0da2e628c1eba9a8b926
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-05-23 13:44:04 +0700

    update response section

--- src/pages/developers.tsx
+++ src/pages/developers.tsx
@@ -14,34 +14,43 @@ const Items = ({ name, description, apiReference }: Props) => {
       <h2 className="dev_title">{name}</h2>
       <p className="dev_desc">{description}</p>
       {apiReference?.map((item: any, index: number) => {
-        const responseArray = Object.entries(item.response);
         return (
           <div className="dev_ref" key={index}>
+            <h3>{item.name}</h3>
             <div className="dev_endpoint">
               <span>{item.method}</span>
               <span>{item.endpoint}</span>
             </div>
+            {item.params && (
+              <div className="dev_params">
+                <h4>Params</h4>
+                <table className="dev_params_table">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Type</th>
+                      <th>Description</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {item.params.map((param: any, index: number) => {
+                      return (
+                        <tr key={index}>
+                          <td>{param.name}</td>
+                          <td>{param.type}</td>
+                          <td>{param.description}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            )}
             <div className="dev_response">
-              <table className="dev_response_table">
-                <thead>
-                  <tr>
-                    <th>Attribute</th>
-                    <th>Type</th>
-                    <th>Description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {responseArray.map((response: any) => {
-                    return (
-                      <tr key={response[0]}>
-                        <td>{response[0]}</td>
-                        <td>{response[1].type}</td>
-                        <td>{response[1].description}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+              <h4>Response</h4>
+              <pre>
+                <code>{JSON.stringify(item.response, null, 4).replace(/"/g, "")}</code>
+              </pre>
             </div>
           </div>
         );
@@ -57,18 +66,15 @@ const Developer = () => {
         <Container style={{ marginTop: "5rem" }} isSmall>
           <Section title="For Developers">
             <p style={{ paddingBottom: "1.5rem" }}>
-              Pages and API created by{" "}
-              <a href="https://dhafit.xyz/" target="_blank" rel="noreferrer">
-                Dhafit Farenza
-              </a>
-              . All media are obtained from{" "}
-              <a href="https://www.jype.com/" target="_blank" rel="noreferrer">
+              All media are obtained from{" "}
+              <a href="https://www.jype.com/" target="_blank" rel="noreferrer nofollow">
                 JYPE
               </a>
               {" & "}
-              <a href="https://wmg.jp/" target="_blank" rel="noreferrer">
+              <a href="https://wmg.jp/" target="_blank" rel="noreferrer nofollow">
                 Sony Music
               </a>
+              .
             </p>
             <div className="apiReference">
               {developersPage.map((item, index) => {
