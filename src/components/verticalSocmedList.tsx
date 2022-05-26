5153fd553f957622134889df648d0f3ef57eda8f
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-05-26 17:45:01 +0700

    add aria-label

--- src/components/verticalSocmedList.tsx
+++ src/components/verticalSocmedList.tsx
@@ -5,27 +5,28 @@ import { FaTiktok } from "react-icons/fa";
 interface LinksProps {
   href: string;
   icon: JSX.Element;
+  ariaLabel: string;
 }
 
 const twiceKorea: LinksProps[] = [
-  { href: "https://www.instagram.com/twicetagram/", icon: <AiOutlineInstagram /> },
-  { href: "https://twitter.com/jypetwice", icon: <AiOutlineTwitter /> },
-  { href: "https://www.youtube.com/c/TWICE", icon: <BsYoutube /> },
-  { href: "https://www.tiktok.com/@twice_tiktok_official", icon: <FaTiktok /> },
+  { href: "https://www.instagram.com/twicetagram/", icon: <AiOutlineInstagram />, ariaLabel: "TWICE Korea's Instagram" },
+  { href: "https://twitter.com/jypetwice", icon: <AiOutlineTwitter />, ariaLabel: "TWICE Korea's Twitter" },
+  { href: "https://www.youtube.com/c/TWICE", icon: <BsYoutube />, ariaLabel: "TWICE Korea's Youtube" },
+  { href: "https://www.tiktok.com/@twice_tiktok_official", icon: <FaTiktok />, ariaLabel: "TWICE Korea's Tiktok" },
 ];
 
 const twiceJapan: LinksProps[] = [
-  { href: "https://www.instagram.com/jypetwice_japan/", icon: <AiOutlineInstagram /> },
-  { href: "https://twitter.com/JYPETWICE_JAPAN", icon: <AiOutlineTwitter /> },
-  { href: "https://www.youtube.com/channel/UCCRb6nYKaT8tzLA8CwDdUtw", icon: <BsYoutube /> },
-  { href: "https://www.tiktok.com/@twice_tiktok_officialjp", icon: <FaTiktok /> },
+  { href: "https://www.instagram.com/jypetwice_japan/", icon: <AiOutlineInstagram />, ariaLabel: "TWICE Japan's Instagram" },
+  { href: "https://twitter.com/JYPETWICE_JAPAN", icon: <AiOutlineTwitter />, ariaLabel: "TWICE Japan's Twitter" },
+  { href: "https://www.youtube.com/channel/UCCRb6nYKaT8tzLA8CwDdUtw", icon: <BsYoutube />, ariaLabel: "TWICE Japan's Youtube" },
+  { href: "https://www.tiktok.com/@twice_tiktok_officialjp", icon: <FaTiktok />, ariaLabel: "TWICE Japan's TikTok" },
 ];
 
 const mappedList = (arrayElement: LinksProps[]) => {
   return arrayElement.map((item: LinksProps, index: number) => {
     return (
       <li key={index}>
-        <a href={item.href} target="_blank" rel="noreferrer nofollow">
+        <a href={item.href} aria-label={item.ariaLabel} target="_blank" rel="noreferrer nofollow">
           {item.icon}
         </a>
       </li>
